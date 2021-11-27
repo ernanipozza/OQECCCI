2668fd156ca11c9fbfabd958679f556ec2bc6aab
ernanipozza <93284793+ernanipozza@users.noreply.github.com>
2021-11-26 21:38:37 -0300

    Update receitas.js

--- receitas.js
+++ receitas.js
@@ -532,7 +532,10 @@ export const receitasTodas = [{
 				"10": "ovos",
 				"11": "orégano",
 				"12": "oregano",
-				"13": "fermento quimico"
+				"13": "fermento quimico",
+				"14": "carne",
+				"15": "carne moída",
+				"16": "carne moida"
 			},
 			"modoPreparo": "Bata no liquidificador os ovos, o leite, o óleo, e acrescente a farinha de trigo aos poucos. Após acrescentar toda a farinha de trigo, adicione sal a gosto. Misture a massa até obter uma consistência cremosa. Com um papel-toalha, espalhe óleo por toda a frigideira e despeje uma concha de massa. Faça movimentos circulares para que a massa se espalhe por toda a frigideira. Espere até a massa soltar do fundo e vire-a para fritar do outro lado. Em uma panela, doure a cebola com o óleo e acrescente a carne. Deixe cozinhar até que saia água da carne, diminua o fogo e tampe. Acrescente o tomate picado e tampe novamente. Deixe cozinhar por mais 3 minutos e misture. Acrescente o extrato de tomate e temperos a gosto. Deixe cozinhar por mais 10 minutos. Quando o molho engrossar, desligue o fogo. Deixe esfriar o molho, acrescente o creme de leite e misture bem. Quando estiver bem homogêneo, leve novamente ao fogo e deixe cozinhar em fogo baixo por mais 5 minutos. Recheie a panqueca com uma fatia de muçarela, uma porção de carne e enrole.Faça esse processo com todas as panquecas. Despeje um pouco de caldo no fundo de um refratário, para untar. Disponha as panquecas já prontas no refratário e despeje sobre elas o restante do molho. Polvilhe queijo ralado sobre as panquecas. Leve ao forno para gratinar, em fogo médio, por 20 minutos ou até que o queijo esteja derretido.",
 			"link_imagem":"assets/images/29 - Panqueca de Carne.jpg",
